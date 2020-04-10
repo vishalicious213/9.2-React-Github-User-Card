64830edae2af9df961f13abb0170d9f0bf314428
vishalicious213 <vishalicious213@gmail.com>
2020-04-10 17:52:49 -0400

    converted App from a function component to a class component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Usercard from './components/Usercard';
 import Followers from './components/Followers';
 import Search from './components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <Search />
-      <header className="App-header">
-        React GitHub User Card
-      </header>
-      <Usercard />
-      <Followers />
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Search />
+          <header className="App-header">
+            React GitHub User Card
+          </header>
+          <Usercard />
+          <Followers />
+        </div>
+      );
+    }
 }
 
 export default App;
