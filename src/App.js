902b598399a1e02d2bfa4700237f4d28132b6480
vishalicious213 <vishalicious213@gmail.com>
2020-04-12 04:21:50 -0400

    puzzling out search function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
           user: 'vishalicious213',
           userData: [],
           followersData: [],
+          search: '',
         }
     }
 
@@ -26,32 +27,62 @@ class App extends React.Component {
           .catch(error => console.log('Error: ', error));
       }
 
-      const UpdateUser = (props) => {
-        this.setState({user: props})
-      }
+      // const UpdateUser = (props) => {
+      //   this.setState({user: props})
+      // }
 
       const GetFollowersData = () => {
         axios
           .get(`https://non-cors.herokuapp.com/https://api.github.com/users/${this.state.user}/followers`)
           .then(results => {
             // console.log(results);
+            console.log('Getting followers again')
             this.setState({followersData: results.data});
           })
           .catch(error => console.log('Error: ', error));
       }
 
+
       GetUserData();
       GetFollowersData();
     }
+
+    // componentDidUpdate() {
+      getSearchText = event => {
+        event.preventDefault();
+        console.log('typed: ', event.target.value);
+        this.setState({search: event.target.value});
+        console.log('searchState: ', this.state.search);
+      }
+
+      submitSearch = event => {
+        event.preventDefault();
+        console.log(event.target.value);
+        console.log('searchState from submitSearch: ', this.state.search);
+        console.log('user before: ', this.state.user);
+        this.setState({user: this.state.search});
+        console.log('user after: ', this.state.user);
+        // // setQuery(search);
+        // this.setState(search: '');
+        // // setSearch('')
+      }
+
+    // }
 
 
     render() {
       // console.log('Userdata: ', this.state.userData)
       // console.log(this.state.userData.name)
-      console.log('Followersdata: ', this.state.followersData)
+      // console.log('Followersdata: ', this.state.followersData)
       return (
         <div className="App">
           <Search />
+
+      <form className='search-form' onSubmit={this.submitSearch}>
+        <input className='search-bar' type='text' onChange={this.getSearchText} placeholder='Enter username'/>
+        <button className='search-button' type='submit'>Search</button>
+      </form>
+
           <header className="App-header">
             React GitHub User Card
           </header>
