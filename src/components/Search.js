3fb470d2dce8d1146122249d9408df93b918c81b
vishalicious213 <vishalicious213@gmail.com>
2020-04-13 19:01:51 -0400

    cleaned up comments in code to deal with a minification error

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import './Search.css';
 
-// class Search extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {};
-//     }
-
 const Search = (props) => {
         return (
-            // <div>Search Component</div>
-
             <form className='search-form' onSubmit={props.handleSubmit}>
                 <i className='fab fa-github' title='See another GitHub user'></i>
                 <input
